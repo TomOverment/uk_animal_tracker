ae6f17d5e82e72e537728e6c87693ee6a9ac44ce
TomOverment <tomozart22@outlook.com>
2026-01-19 14:36:13 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="UK Animal Tracker", layout="wide")
+st.title("UK Animal Tracker")
+st.write("Use the sidebar to open the Mammals Dashboard page.")
